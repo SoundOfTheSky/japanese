485084a76912424268cff22f6a8ef9b7ae496cf1
nikita.tkachiov <nikita.tkachiov@edgecenter.ru>
2025-06-05 17:38:50 +0300

    Initial

---
+++ src/wanikani.ts
@@ -0,0 +1,163 @@
+import { write } from 'bun'
+
+import { removeWholeSentenceWithSubstring } from './utilities'
+
+export type WKResponse<T> = {
+  object: string
+  url: string
+  pages: {
+    per_page: number
+    next_url?: string
+    previous_url?: string
+  }
+  total_count: number
+  data_updated_at: string
+  data: WKObject<T>[]
+}
+export type WKObject<T> = {
+  id: number
+  object: string
+  url: string
+  data_updated_at: string
+  data: T
+}
+export type WKSubject = {
+  auxiliary_meanings: {
+    meaning: string
+    type: 'whitelist' | 'blacklist'
+  }[]
+  created_at: string
+  document_url: string
+  hidden_at?: string
+  lesson_position: number
+  level: number
+  meaning_mnemonic: string
+  meanings: {
+    meaning: string
+    primary: boolean
+    accepted_answer: boolean
+  }[]
+  slug: string
+  spaced_repetition_system_id: number
+}
+export type WKRadical = WKSubject & {
+  amalgamation_subject_ids: number[]
+  characters?: string
+  character_images: {
+    url: string
+    content_type: 'image/png' | 'image/svg+xml'
+    metadata: unknown
+    inline_styles?: boolean
+    color?: string
+    dimensions?: string
+    style_name?: string
+  }[]
+}
+export type WKKanji = WKSubject & {
+  characters: string
+  amalgamation_subject_ids: number[]
+  component_subject_ids: number[]
+  meaning_hint?: string
+  reading_hint?: string
+  reading_mnemonic: string
+  readings: {
+    reading: string
+    primary: boolean
+    accepted_answer: boolean
+    type: 'kunyomi' | 'nanori' | 'onyomi'
+  }[]
+  visually_similar_subject_ids: number[]
+}
+export type WKKana = WKSubject & {
+  characters: string
+  context_sentences: {
+    en: string
+    ja: string
+  }[]
+  meaning_mnemonic: string
+  parts_of_speech: string[]
+  pronunciation_audios: {
+    url: string
+    content_type: 'audio/mpeg' | 'audio/ogg'
+    metadata: {
+      gender: string
+      source_id: number
+      pronunciation: string
+      voice_actor_id: number
+      voice_actor_name: string
+      voice_description: string
+    }
+  }[]
+}
+export type WKVocab = WKKana & {
+  characters: string
+  component_subject_ids: number[]
+  readings: {
+    reading: string
+    primary: boolean
+    accepted_answer: boolean
+  }[]
+  reading_mnemonic: string
+}
+export type WKAnySubject = WKRadical | WKKanji | WKKana | WKVocab
+
+export async function downloadWK() {
+  const subjects: WKObject<WKAnySubject>[] = []
+  let nextUrl: string | undefined = 'https://api.wanikani.com/v2/subjects'
+  while (nextUrl) {
+    console.log(nextUrl)
+    const json = (await fetch(nextUrl, {
+      headers: {
+        Authorization: 'Bearer ' + process.env.WK,
+      },
+    }).then((x) => x.json())) as WKResponse<WKAnySubject>
+    try {
+      nextUrl = json.pages.next_url
+      subjects.push(...json.data)
+    } catch (error) {
+      console.error(json)
+      throw error
+    }
+  }
+  await write('assets/WK.json', JSON.stringify(subjects, undefined, 2))
+}
+export const WK: WKObject<WKAnySubject>[] = []
+
+/**
+ * Sentences that include this substrings will be cut.
+ *
+ * WaniKani has a lot of unnecessary explanations that tend to repeat
+ * thousands of times across many mnemonics. Since we want only mnemonics
+ * without any additional explanations (and visual clutter),
+ * we will be removing them.
+ *
+ * If there are no mnemonic, it means it either not on wanikani,
+ * or consists entirely of unneccessary explanations. Check WK level tag.
+ *
+ * False positives may apply. If you see one, report it.
+ */
+const unnecessary = [
+  "anji portion uses the kun'yomi reading",
+  'are exactly the same',
+  'consists of a kanji with hiragana',
+  'ends with an う',
+  'hiragana attached',
+  'his is a jukugo word',
+  'ince this word',
+  'kanji and the word are exactly the same',
+  'know the readings',
+  'mnemonic to help',
+  'on your own',
+  'ord is made up',
+  'ou know the readings',
+  'ou should be able',
+  'same as the one you learned',
+  'share meanings as well',
+  'share same meaning',
+]
+
+export function cutUnnecessary(text: string) {
+  for (let index = 0; index < unnecessary.length; index++)
+    text = removeWholeSentenceWithSubstring(text, unnecessary[index]!)
+  return text
+}
---
+++ src/yomitan/index.ts
@@ -0,0 +1,45 @@
+import { file } from 'bun'
+
+import { downloadWK, WK, WKAnySubject, WKObject } from '../wanikani'
+
+import { generateKanji } from './kanji'
+import { generateTerms } from './term'
+import YomitanDictionary from './yomitan-dictionary'
+
+const dictionary = new YomitanDictionary({
+  author: 'SoundOfTheSky',
+  description: '',
+  downloadUrl:
+    'https://github.com/SoundOfTheSky/yomitan-sky/raw/refs/heads/main/dist/Sky.zip',
+  format: 3,
+  indexUrl:
+    'https://github.com/SoundOfTheSky/yomitan-sky/raw/refs/heads/main/dist/Sky.json',
+  isUpdatable: true,
+  revision: '1.4.1-' + new Date().toISOString().split('T')[0],
+  sequenced: true,
+  sourceLanguage: 'ja',
+  targetLanguage: 'en',
+  title: 'Sky',
+  attribution: 'This dictionary simply combines following sources:',
+  url: 'https://github.com/SoundOfTheSky/yomitan-sky',
+})
+
+// === Download assets and merge ===
+if (!(await file('assets/WK.json').exists())) await downloadWK()
+dictionary.index.attribution += `\n\n=== WaniKani ===\nLink: https://www.wanikani.com\n© Tofugu LLC`
+WK.push(...((await file('assets/WK.json').json()) as WKObject<WKAnySubject>[]))
+await dictionary.merge(
+  'jitendex',
+  'https://github.com/stephenmk/stephenmk.github.io/releases/latest/download/jitendex-yomitan.zip',
+)
+await dictionary.merge(
+  'kanjidic',
+  'https://github.com/yomidevs/jmdict-yomitan/releases/latest/download/KANJIDIC_english.zip',
+)
+
+// === Process ===
+dictionary.tag.push(['wk', 'misc', 0, 'WaniKani level', 0])
+generateTerms(dictionary)
+generateKanji(dictionary)
+
+await dictionary.save()
